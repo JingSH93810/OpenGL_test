c4d7627f188d149a4061b300f320836b9ec5c086
JingSH93810 <jingshihao0810@gmail.com>
2016-01-25 12:40:33 +0800

    add create circle profile manually

--- OpenGL_test/main.cpp
+++ OpenGL_test/main.cpp
@@ -1,89 +1,305 @@
-#include <string.h>
-#include <math.h>
-#include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <math.h> 
+#include <stdio.h>
 #include <iostream>
-#include "ProfileList.h"
-
 using namespace std;
 
-#define PI 3.1415926535898
-#define R 2 
-#define drawOneLine(x1,y1,x2,y2)  glBegin(GL_LINES);  \
-   glVertex2f ((x1),(y1)); glVertex2f ((x2),(y2)); glEnd();
-static double year = 0, day = 0;
-static ProfileList profileList;
-static GLdouble translateY = 0;
-static int profileNumber = 0;
-
-GLdouble* createNewProfile(GLdouble translateY);
-
-void init(void)
+#define PI 3.1415926
+/* 每个图形最多的顶点数 */
+#define MAX_VERTEX 10
+/* 画的图形最多的个数 */
+#define MAX_PLOY 10
+/* 窗口长宽的一半 */
+int halfWidth, halfHeight;
+/* 绘制多边形的起始标志，0是开始绘制，1是结束绘制，初始为-1 */
+int drawStatus = -1;
+/* 多边形结构体 */
+struct polygon {
+	/* 顶点坐标 */
+	int x[MAX_VERTEX];
+	int y[MAX_VERTEX];
+	/* 定义第几个顶点 */
+	int verNum;
+	GLubyte color[3];
+};
+int radius_X[2];
+int radius_Y[2];
+int circle_count = 0;
+struct circle
 {
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_FLAT);
+	/*圆心坐标*/
+	double x;
+	double y;
+	/*圆半径*/
+	double radius;
+	GLubyte color[3];
+};
+circle circle1;
+/* 各种颜色 */
+GLubyte border[3] = { 0, 0, 0 };
+GLubyte grey[3] = { 195, 195, 195 };
+GLubyte yellow[3] = { 255, 243, 0 };
+GLubyte red[3] = { 237, 28, 36 };
+GLubyte darkGrey[3] = { 126, 126, 126 };
+GLubyte white[3] = { 255, 255, 255 };
+GLubyte startBtn[3] = { 10, 10, 10 };
+GLubyte endBtn[3] = { 20, 20, 20 };
+/* 当前颜色 */
+GLubyte nowColor[3] = { 0, 0, 0 };
+/* 声明多边形数组 */
+polygon polygons[MAX_PLOY];
+/* 记录画了几个多边形 */
+int con = 0;
+int day = 0;
+
+double calculateRadius(int x1, int y1, int x2, int y2) {
+	double radius = (sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)) / 2.0);
+	return radius;
 }
 
-void drawCircle() {
-	GLint circle_points = 100, i = 0;
-	glBegin(GL_LINE_LOOP);
-	for (int i = 0; i < circle_points; i++)
+//画弧线，相对偏移量XY，开始的弧度，结束的弧度，半径
+void glArc(double x, double y, double start_angle, double end_angle, double radius)
+{
+	//开始绘制曲线
+	glBegin(GL_LINE_STRIP);
+	//每次画增加的弧度
+	double delta_angle = PI / 180;
+	//画圆弧
+	for (double i = start_angle; i <= end_angle; i += delta_angle)
 	{
-		double angle = 2 * PI*i / circle_points;
-		glVertex3d(2*cos(angle), 2*sin(angle), 0);
+		//绝对定位加三角函数值
+		double vx = x + radius * cos(i);
+		double vy = y + radius*sin(i);
+		glVertex2d(vx, vy);
 	}
+	//结束绘画
 	glEnd();
 }
 
-void generateSurface(GLdouble* circle1, GLdouble* circle2) {
 
-	glColor3f(1.0, 0.0, 0.0);
-	for (int k = 0; k < 150; k += 3)
-	{
-		glVertex3d(circle1[k], circle1[k + 1], circle1[k + 2]);
-		glVertex3d(circle2[k], circle2[k + 1], circle2[k + 2]);
-	}
-	glVertex3d(circle1[0], circle1[1], circle1[2]);
-	glVertex3d(circle2[0], circle2[1], circle2[2]);
-
+//画圆
+void glCircle(double x, double y, double radius)
+{
+	//画全圆
+	glArc(x, y, 0, 2 * PI, radius);
 }
 
+/* 绘制多边形 */
+void glPolygons()
+{
+	if (con >= 0) {
+		for (int i = 0; i <= con; i++) {
+			/* 取到这个多边形 */
+			polygon poly = polygons[i];
+			/* 画封闭线 */
+			glBegin(GL_LINE_LOOP);
+			int num = poly.verNum;
+			printf("num:%d\n", num);
+			for (int j = 0; j < num; j++)
+			{
+				glColor3ub(poly.color[0], poly.color[1], poly.color[2]);
+				glVertex2d(poly.x[j] - halfWidth, halfHeight - poly.y[j]);
+				printf("polyx:%d,polyy:%d", poly.x[j], poly.y[j]);
+			}
+			/* 结束画线 */
+			glEnd();
+			/* 刷新 */
+			glFlush();
+		}
+	}
+}
+
+/* 绘制填充的圆形 */
+void glColorCircle(int x, int y, int R, GLubyte color[3])
+{
+	/* 开始绘制曲线 */
+	glBegin(GL_POLYGON);
+	/* 设置颜色 */
+	glColor3ub(color[0], color[1], color[2]);
+	/* 每次画增加的弧度 */
+	double delta_angle = PI / 180;
+	/* 画圆弧 */
+	for (double i = 0; i <= 2 * PI; i += delta_angle)
+	{
+		/* 绝对定位加三角函数值 */
+		double	vx = x + R * cos(i);
+		double	vy = y + R*sin(i);
+		glVertex2d(vx, vy);
+	}
+	/* 结束绘画 */
+	glEnd();
+	glFlush();
+}
+
+//画线，传入两点坐标
+void glLine(int x1, int y1, int x2, int y2) {
+	//画封闭线
+	glBegin(GL_LINE_STRIP);
+	//一点
+	glVertex2d(x1, y1);
+	//二点
+	glVertex2d(x2, y2);
+	//结束画线
+	glEnd();
+}
+
+/* 画点 */
+void glPoints(int x, int y) {
+	glBegin(GL_POINTS);
+	/* 点直接设置为黑色 */
+	glColor3ub(0, 0, 0);
+	glPointSize(100);
+	glVertex2d(x, y);
+	glEnd();
+	glFlush();
+}
+
+//函数用来画图
 void display(void)
 {
+	//GL_COLOR_BUFFER_BIT表示清除颜色
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 1.0, 1.0);
+	//设置画线颜色
+	glColor3f(0.5, 0.5, 0.5);
 
+	/* 绘制取色盘 */
+	glColorCircle(-280, 280, 10, red);
+	glColorCircle(-250, 280, 10, yellow);
+	glColorCircle(-220, 280, 10, grey);
+	glColorCircle(-180, 280, 10, nowColor);
+	glColorCircle(250, 280, 10, startBtn);
+	glColorCircle(280, 280, 10, endBtn);
 
-	glRotated(day, 1.0, 0.0, 0.0);
-	glRotated(year, 0.0, 1.0, 0.0);
-
-	glPushMatrix();
-	
-	glBegin(GL_TRIANGLE_STRIP);
-	Node* p = profileList.GetHead();
-	while (p != NULL && p->next != NULL)
+	for (int i = 0; i < circle_count; i++)
 	{
-		GLdouble* circle1 = p->vertexArray;
-		GLdouble* circle2 = p->next->vertexArray;
-		generateSurface(circle1, circle2);
-		p = p->next;
+		glColorCircle(radius_X[i], radius_Y[i], 5, red);
 	}
-	glEnd();
-
-	glPopMatrix();
-
-	glutSwapBuffers();
+	//保证前面的OpenGL命令立即执行，而不是让它们在缓冲区中等待 
+	/* 绘制多边形 */
+	//if(drawStatus == 1) glPolygons();
+	if (drawStatus == 1) {
+		glPushMatrix();
+		glRotated(day, 1, 0, 0);
+		glColorCircle(radius_X[0], radius_Y[0], 5, red);
+		glColorCircle(radius_X[1], radius_Y[1], 5, red);
+		glColor3b(0,0,0);
+		glCircle(circle1.x, circle1.y, circle1.radius);
+		glPopMatrix();
+	}
+	glFlush();
 }
 
-void reshape(int w, int h)
+
+//窗口大小变化时调用的函数
+void ChangeSize(GLsizei w, GLsizei h)
 {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	//避免高度为0
+	if (h == 0) {
+		h = 1;
+	}
+	//定义视口大小，宽高一致  
+	glViewport(0, 0, w, h);
+	int half = 300;
+	/* 定义宽高的一半 */
+	halfHeight = 300;
+	halfWidth = 300;
+	//重置坐标系统，使投影变换复位
 	glMatrixMode(GL_PROJECTION);
+	//将当前的用户坐标系的原点移到了屏幕中心
 	glLoadIdentity();
-	gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 4.0, 20.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	//定义正交视域体  
+	if (w<h) {
+		//如果高度大于宽度，则将高度视角扩大，图形显示居中
+		glOrtho(-half, half, -half*h / w, half*h / w, -half, half);
+	}
+	else {
+		//如果宽度大于高度，则将宽度视角扩大，图形显示居中
+		glOrtho(-half*w / h, half*w / h, -half, half, -half, half);
+	}
+
+}
+/* 判断两个颜色是否相等 */
+bool sameColor(GLubyte color1[3], GLubyte color2[3])
+{
+	if (color1[0] - color2[0] < 5 && color1[1] - color2[1] < 5 && color1[2] - color2[1] < 5)
+	{
+		return(true);
+	}
+	else {
+		return(false);
+	}
+}
+
+
+
+/*鼠标点击事件 */
+void mouseClick(int btn, int state, int x, int y)
+{
+	/* 选项卡的分界范围 */
+	int optionHeight = 40;
+	int optionWidth = 250;
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		/* 如果在颜色选项卡中点击 */
+		if (y < optionHeight && x < optionWidth)
+		{
+			glReadPixels(x, 2 * halfHeight - y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, &nowColor);
+			printf("r:%d,g:%d,b:%d\n", nowColor[0], nowColor[1], nowColor[2]);
+			/* 设置绘图颜色并显示当前取色板颜色 */
+			glColorCircle(-180, 280, 10, nowColor);
+			/* 如果点击的是右侧选项按钮 */
+		}
+		else if (y < optionHeight && x > optionWidth) {
+			//取当前的点
+			GLubyte color[3];
+			glReadPixels(x, 2 * halfHeight - y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, &color);
+			printf("r:%d,g:%d,b:%d\n", color[0], color[1], color[2]);
+			/* 如果点击了开始绘制的按钮 */
+			if (sameColor(color, startBtn)) {
+				drawStatus = 0;
+				/* 开始画一个图形，顶点个数置零 */
+				polygons[con].verNum = 0;
+				printf("drawStatus:%d\n", drawStatus);
+				/* 如果点击了结束绘制的按钮 */
+			}
+			else if (sameColor(color, endBtn)) {
+				drawStatus = 1;
+				circle_count = 0;
+				printf("x1:%d, y1:%d, x2:%d, y2:%d\n", radius_X[0], radius_Y[0], radius_X[1], radius_Y[1]);
+
+				circle1.x = (radius_X[1] + radius_X[0]) / 2.0;
+				printf("origin x:%f\n", circle1.x);
+				circle1.y = (radius_Y[1] + radius_Y[0]) / 2.0;
+				printf("origin y:%f\n", circle1.y);
+				circle1.radius = calculateRadius(radius_X[0], radius_Y[0], radius_X[1], radius_Y[1]);
+
+
+
+				printf("radius:%f\n", circle1.radius);
+				/* 画的图形个数加一 */
+				con++;
+				printf("drawStatus:%d\n", drawStatus);
+			}
+			/* 如果点击的是下方的绘图页面 */
+		}
+		else {
+			/* 如果当前是正在取点状态 */
+			if (drawStatus == 0)
+			{
+				if (circle_count == 2) {
+					circle_count = 0;
+				}
+				if (circle_count < 2) {
+					radius_X[circle_count] = x - halfWidth;
+					radius_Y[circle_count] = halfHeight - y;
+					glColorCircle(radius_X[circle_count], radius_Y[circle_count], 5, red);
+					circle_count++;
+				}
+
+
+			}
+		}
+	}
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -94,77 +310,33 @@ void keyboard(unsigned char key, int x, int y)
 		cout << day << endl;
 		glutPostRedisplay();
 		break;
-	case 'f':
-		day = day - 1;
-		cout << day << endl;
-		glutPostRedisplay();
-		break;
-	case 'c':
-		year = year + 1;
-		cout << year << endl;
-		glutPostRedisplay();
-		break;
-	case 'v':
-		year = year - 1;
-		cout << year << endl;
-		glutPostRedisplay();
-		break;
-	case 'r':
-		year = 0;
-		day = 0;
-		cout << year << endl;
-		glutPostRedisplay();
-		break;
-	case 'q': 
-	{
-		translateY = translateY + 0.5;
-		GLdouble* newProfile = createNewProfile(translateY);
-		if (profileList.GetHead() == NULL)
-		{
-			cout << "no head , create one " << endl;
-			Node* p = new Node(newProfile);
-			profileList.SetHead(p);
-		}
-		else
-		{
-			profileList.Insert(newProfile, profileList.GetNodeNumber() - 1);
-		}
-		cout << translateY << endl;
-		cout << profileList.GetNodeNumber() << endl;
-		glutPostRedisplay();
-		break;
-	}
-		
-	case 27:
-		exit(0);
-		break;
 	}
 }
 
-int main(int argc, char** argv)
-{
+
+//程序入口
+int main(int argc, char *argv[]) {
+	//对GLUT进行初始化，并处理所有的命令行参数
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(300, 100);
-	glutCreateWindow(argv[0]);
-	init();
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
+	//指定RGB颜色模式和单缓冲窗口
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	//定义窗口的位置
+	glutInitWindowPosition(100, 100);
+	//定义窗口的大小
+	glutInitWindowSize(600, 600);
+	//创建窗口，同时为之命名
+	glutCreateWindow("OpenGL");
+	//设置窗口清除颜色为白色
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	//参数为一个函数，绘图时这个函数就会被调用
+	glutDisplayFunc(&display);
+	//参数为一个函数，当窗口大小改变时会被调用
+	glutReshapeFunc(ChangeSize);
+	/*鼠标点击事件，鼠标点击或者松开时调用 */
+	glutMouseFunc(mouseClick);
 	glutKeyboardFunc(keyboard);
+	//该函数让GLUT框架开始运行，所有设置的回调函数开始工作，直到用户终止程序为止
 	glutMainLoop();
+	//程序返回
 	return 0;
-}
-
-GLdouble* createNewProfile(GLdouble translateY) {
-	GLdouble* profile = new GLdouble[150];
-	GLint circle_points = 50, i = 0;
-	for (int i = 0, k = 0; i < circle_points && k < 150; i++, k += 3)
-	{
-		double angle = 2 * PI*i / circle_points;
-		profile[k] = 2 * cos(angle);
-		profile[k + 1] = translateY;
-		profile[k + 2] = 2 * sin(angle);;
-	}
-	return profile;
 }
